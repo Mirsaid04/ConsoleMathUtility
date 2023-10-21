5a06a316346bd2cc01685130892723f70ced9511
Mirsaid04 <mirsaidd04@gmail.com>
2023-10-21 18:42:31 +0900

    CODE RUB : Delete Useless Words

--- ConsoleMathUtility/Classes/ValueManipulator.cs
+++ ConsoleMathUtility/Classes/ValueManipulator.cs
@@ -4,7 +4,7 @@
     {
         public void ShowMenu()
         {
-            Console.WriteLine("Welcome to my Calculator Application.\n______Menu______n");
+            Console.WriteLine("Welcome to my Calculator Application.\n______Menu______\n");
             Console.Write("1.Add two numbers \n2.Add list of numbers. \n3.Multiplication Table. \n4.Choice: ");
         }
 
